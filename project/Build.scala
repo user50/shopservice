87d7011234d1ebfd3f2959fb9bcc48af2c6604f8
oleg.oginskiy <oleg@themidnightcoders.com>
2013-08-31 08:18:41 +0300

    adapted for play 2.1.x

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,18 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "project2608"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "testApp"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+  )
 
 }
