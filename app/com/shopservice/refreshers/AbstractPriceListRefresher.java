92a49e9f3649b53fd4c8773391178ee1c8809a9e
oleg.oginskiy <oleg@themidnightcoders.com>
2013-10-22 16:02:07 +0300

    added ability to generate and download files for specified sites

--- app/com/shopservice/refreshers/AbstractPriceListRefresher.java
+++ app/com/shopservice/refreshers/AbstractPriceListRefresher.java
@@ -10,10 +10,10 @@ import java.util.Set;
 
 public abstract class AbstractPriceListRefresher implements PriceListRefresher {
 
-    protected Set<String> getProductIds(String clientId) throws SQLException {
+    protected Set<String> getProductIds(String clientId, int siteId) throws SQLException {
         Set<String> setOfProductIds = new HashSet<String>();
 
-        for (ProductEntry productEntry : ProductEntry.findSelected(clientId))
+        for (ProductEntry productEntry : ProductEntry.findSelected(clientId, siteId))
             setOfProductIds.add(productEntry.productId);
 
         return setOfProductIds;
