1d22fcf60661f5f6492dde3a44aa512e763b82e6
oleg.oginskiy <oleg@themidnightcoders.com>
2013-10-23 11:39:06 +0300

    fixed bug

--- app/com/shopservice/domain/Site2Product.java
+++ app/com/shopservice/domain/Site2Product.java
@@ -32,21 +32,45 @@ public class Site2Product {
 
     public static void set(String productId, int siteId, Boolean checked) {
 
-        Site2Product site2Product = Site2Product.get( productId, siteId );
-        if (site2Product == null)
-            site2Product = new Site2Product();
-
-        site2Product.checked = checked;
-        site2Product.site = Ebean.find(Site.class, siteId);
-        site2Product.productEntry = Ebean.find(ProductEntry.class, productId);
-
-        Ebean.save(site2Product);
+        if (checked)
+            Ebean.createSqlUpdate("INSERT INTO site2product (`site_id`, `product_entry_id`) " +
+                    "SELECT ?, product_entry.id FROM product_entry " +
+                    "LEFT JOIN site2product ON site2product.product_entry_id = product_entry.id AND site2product.site_id = ? " +
+                    "WHERE product_entry.id = ? AND site2product.id IS NULL")
+                    .setParameter(1, siteId)
+                    .setParameter(2, siteId)
+                    .setParameter(3, productId)
+                    .execute();
+        else
+            Ebean.createSqlUpdate("DELETE site2product.* FROM product_entry " +
+                    "LEFT JOIN site2product ON site2product.product_entry_id = product_entry.id AND site2product.site_id = ? " +
+                    "WHERE product_entry.id = ? AND site2product.id IS NOT NULL")
+                    .setParameter(1, siteId)
+                    .setParameter(2, productId)
+                    .execute();
     }
 
     public static void set(String clientId, String categoryId, int siteId, Boolean checked) {
 
-        for (ProductEntry productEntry : ProductEntry.get(clientId, categoryId))
-            set(productEntry.id, siteId, checked);
+        if (checked)
+            Ebean.createSqlUpdate("INSERT INTO site2product (`site_id`, `product_entry_id`) " +
+                    "SELECT ?, product_entry.id FROM product_entry " +
+                    "LEFT JOIN site2product ON site2product.product_entry_id = product_entry.id AND site2product.site_id = ? " +
+                    "WHERE client_settings_id = ? AND category_id = ? AND site2product.id IS NULL")
+                    .setParameter(1, siteId)
+                    .setParameter(2, siteId)
+                    .setParameter(3, clientId)
+                    .setParameter(4, categoryId)
+                    .execute();
+        else
+            Ebean.createSqlUpdate("DELETE site2product.* FROM product_entry " +
+                    "LEFT JOIN site2product ON site2product.product_entry_id = product_entry.id AND site2product.site_id = ? " +
+                    "WHERE client_settings_id = ? AND category_id = ? AND site2product.id IS NOT NULL")
+                    .setParameter(1, siteId)
+                    .setParameter(2, clientId)
+                    .setParameter(3, categoryId)
+                    .execute();
+
 
     }
 }
