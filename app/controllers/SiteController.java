e5812b6f4a399e656b83e8e40b7a3fd2db021738
oleg.oginskiy <oleg@themidnightcoders.com>
2013-11-06 12:04:44 +0200

    unbind tables ClientSettings and Site

--- app/controllers/SiteController.java
+++ app/controllers/SiteController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import com.shopservice.domain.ClientSettings;
 import com.shopservice.domain.Site;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -8,30 +7,9 @@ import play.mvc.Result;
 
 public class SiteController extends Controller {
 
-    public static Result getSites(String clientId)
+    public static Result getSites()
     {
-        return ok( Json.toJson( Site.get(clientId) ) );
-    }
-
-    public static Result addSite( String clientId )
-    {
-        ClientSettings settings = ClientSettings.findById(clientId);
-        if (settings == null)
-            return badRequest("Client with id "+clientId+" doesn't exist ");
-
-        Site site = Json.fromJson( request().body().asJson(), Site.class );
-
-        if (site.name.isEmpty() )
-            return badRequest("Name cannot be empty");
-
-        if (Site.exist(clientId, site.name))
-            return badRequest("Category with specified name already exists");
-
-        settings.sites.add(site);
-
-        settings.save();
-
-        return ok( Json.toJson(site) );
+        return ok( Json.toJson( Site.get() ) );
     }
 
 }
