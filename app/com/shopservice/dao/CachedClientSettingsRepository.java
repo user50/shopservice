daab16fbb10891b4b9b3b1102bb5201852e35791
oleg.oginskiy <oleg@themidnightcoders.com>
2013-12-19 15:13:47 +0200

    added cached wrapper to ClientSettings

---
+++ app/com/shopservice/dao/CachedClientSettingsRepository.java
@@ -0,0 +1,63 @@
+package com.shopservice.dao;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.shopservice.domain.ClientSettings;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CachedClientSettingsRepository implements ClientSettingsRepository {
+
+    public static final String BASE = "CachedClientSettingsRepository.base";
+
+    ClientSettingsRepository wrapped;
+
+    private Map<String,ClientSettings> clientSettings = new HashMap<String, ClientSettings>();
+
+    @Inject
+    public CachedClientSettingsRepository(@Named(BASE) ClientSettingsRepository wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public ClientSettings findById(String id) {
+        if (!clientSettings.containsKey(id))
+            clientSettings.put(id, wrapped.findById(id));
+
+        return clientSettings.get(id);
+    }
+
+    @Override
+    public List<ClientSettings> getAll() {
+        return wrapped.getAll();
+    }
+
+    @Override
+    public void remove(String id) {
+        wrapped.remove(id);
+        clientSettings.remove(id);
+    }
+
+    @Override
+    public void save(ClientSettings clientSettings) {
+        wrapped.save(clientSettings);
+
+        if (clientSettings.id != null)
+            this.clientSettings.remove(clientSettings.id);
+    }
+
+    @Override
+    public void update(ClientSettings clientSettings) {
+        wrapped.update(clientSettings);
+
+        if (clientSettings.id != null)
+            this.clientSettings.remove(clientSettings.id);
+    }
+
+    @Override
+    public ClientSettings getBySiteName(String siteName) {
+        return wrapped.getBySiteName(siteName);
+    }
+}
--- app/com/shopservice/modules/MServiceModule.java
+++ app/com/shopservice/modules/MServiceModule.java
@@ -1,6 +1,7 @@
 package com.shopservice.modules;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 import com.shopservice.assemblers.ProductAssembler;
 import com.shopservice.dao.*;
 
@@ -12,7 +13,10 @@ public class MServiceModule extends AbstractModule {
         bind(ProductGroupRepository.class).to(EbeanProductGroupRepository.class).in(Singleton.class);
         bind(Group2ProductRepository.class).to(EbeanGroup2ProductRepository.class).in(Singleton.class);
         bind(ProductEntryRepository.class).to(EbeanProductEntryRepository.class).in(Singleton.class);
-        bind(ClientSettingsRepository.class).to(EbeanClientSettingsRepository.class).in(Singleton.class);
+
+        bind(ClientSettingsRepository.class).to(CachedClientSettingsRepository.class).in(Singleton.class);
+        bind(ClientSettingsRepository.class).annotatedWith(Names.named(CachedClientSettingsRepository.BASE)).to(EbeanClientSettingsRepository.class).in(Singleton.class);
+
         bind(ProductAssembler.class).in(Singleton.class);
     }
 }
