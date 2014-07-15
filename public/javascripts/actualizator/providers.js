6b5554ec3e2ee50fadfea2527abd96a9e36809d1
Eugene Lozov <eugene.lozov@sandsiv.com>
2014-07-15 18:41:55 +0300

    added some functional to actualization (add, edit, delete provider ) (cherry picked from commit 7a9c91a)

---
+++ public/javascripts/actualizator/providers.js
@@ -0,0 +1,119 @@
+var app1 = app1 || {};
+
+var clientId = "client1";
+
+var Provider = Backbone.Model.extend();
+
+var Providers = Backbone.Collection.extend({
+    model: Provider,
+
+    url: function(){
+        return '/clients/'+clientId+'/providers';
+    }
+});
+
+var ProviderView = Backbone.View.extend({
+    tagName: 'tr',
+
+    events: {
+        'click .btn-danger' : 'removeThis',
+        'click .btn-warning' : 'editThis'
+    },
+
+    template: _.template($('#providerViewTemplate').html()),
+
+    initialize: function(){
+        this.model.on('change', this.render, this);
+        this.model.on('destroy', this.remove, this);
+    },
+
+    render: function(){
+        var template = this.template(this.model.toJSON());
+        this.$el.html( template );
+        return this;
+    },
+
+    editThis: function(){
+        app1.ProviderPage.renderEditView(this.model);
+    },
+
+    removeThis: function(){
+        var model = this.model;
+        bootbox.dialog({
+            message: "Вы уверены, что хотите удалить поставщика  \"" + this.model.get('name') + "\" ?",
+            buttons: {
+                success: {
+                    label: "Отмена",
+                    className: "btn-default",
+                    callback: function() {
+                    }
+                },
+                danger: {
+                    label: "Удалить",
+                    className: "btn-danger",
+                    callback: function() {
+                        model.destroy();
+                        $.bootstrapGrowl("Поставщик \"" + model.get('name') + "\" был удален!",
+                            {ele: 'body', type: 'success', width: 350});
+                    }
+                }
+            }
+        });
+    }
+});
+
+var ProvidersView = Backbone.View.extend({
+    el : '#providersTable',
+
+    initialize : function () {
+        var tags = this.collection;
+        tags.on('add', this.render, this);
+        tags.on('remove', this.render, this);
+        tags.on('fetch', this.test, this);
+    },
+
+    events: {
+    },
+
+    test: function(){
+        console.log("products are fetched..");
+    },
+
+    addOne : function ( item ) {
+        var view = new ProviderView({model:item});
+        this.$el.append(view.render().el);
+    },
+
+    render: function(){
+        console.log('Rendering of ProviderView...');
+        this.$el.empty();
+
+        if (this.collection.length == 0){
+            var emptyRow = $('<tr/>');
+            emptyRow.append($('<th/>').text('Список поставщиков пуст.'));
+            this.$el.append(emptyRow);
+            return this;
+        }
+
+        this.renderHeader();
+        this.collection.each(this.addOne, this);
+        return this;
+    },
+
+    renderHeader: function(){
+        var thead = $('<thead/>');
+        var tr = $('<tr/>');
+
+        var th = $('<th class="col-md-1"></th>');
+        tr.append(th);
+        tr.append('<th  class="col-md-4">Название поставщика</th>');
+        tr.append('<th class="col-md-4">Источник</th>');
+        tr.append('<th class="col-md-3"></th>');
+        this.$el.append(tr);
+        console.log("Header is rendered...");
+    }
+});
+
+app1.Providers = new Providers();
+app1.Providers.fetch();
+app1.ProvidersView = new ProvidersView({collection: app1.Providers});
---
+++ public/javascripts/actualizator/providersManager.js
@@ -0,0 +1,101 @@
+var app1 = app1 || {};
+
+
+var ProviderPage = Backbone.View.extend({
+    el: '#providers',
+
+    initialize: function(){
+        this.AddProvider = new AddProvider({collection: app1.Providers});
+        this.EditProvider = new EditProvider({model: new Provider({name: '', url: ''})});
+    },
+
+    events: {
+        'click #addProviderBtn': function(){this.AddProvider.clear()}
+    },
+
+    renderEditView: function(model){
+        this.EditProvider.model.set(model.toJSON());
+    }
+
+});
+
+var AddProvider = Backbone.View.extend({
+    el: '#addProviderModal',
+
+    initialize: function() {
+    },
+
+    events: {
+        'click #saveNewProvider' : 'addProvider'
+    },
+
+    addProvider: function(){
+        var name = this.$el.find('#providerNameInput').val();
+        var url = this.$el.find('#providerUrlInput').val();
+
+        var newProvider = new Provider({name: name, url: url});
+
+        this.collection.create(newProvider, {wait: true,
+            error: function(model, response){
+                $.bootstrapGrowl("Ошибка! " + errorMessages[response.responseJSON.code],
+                    {ele: '#addProviderModal', type: 'danger', width: 350});
+            },
+            success: function(model, response){
+                $.bootstrapGrowl("Изменения успешно сохранены!",
+                    {ele: '#addProviderModal', type: 'success', width: 350});
+            }});
+    },
+
+    clear: function(){
+        console.log('clear form for creating a provider');
+        this.$el.find('#providerNameInput').val('');
+        this.$el.find('#providerUrlInput').val('');
+    }
+});
+
+var EditProvider = Backbone.View.extend({
+    el: '#editProviderModal',
+
+    initialize: function(){
+        var tags = this.model;
+        tags.on('change', this.render, this);
+    },
+
+    events: {
+        'click #editProvider': 'editGroup'
+    },
+
+    editGroup: function(){
+        var changedProvider = app1.Providers.get(this.model.id);
+
+        var changedName = this.$el.find('#providerNameInputEdit').val();
+        var changedUrl = this.$el.find('#providerUrlInputEdit').val();
+
+        changedProvider.set('name', changedName);
+        changedProvider.set('url', changedUrl);
+
+        var provider = changedProvider.toJSON();
+
+        changedProvider.save(provider, {wait: true,
+            error: function(model, response){
+                $.bootstrapGrowl("Ошибка! " + errorMessages[response.responseJSON.code],
+                    {ele: '#editProviderModal', type: 'danger', width: 350});
+            },
+            success: function(model, response){
+                $.bootstrapGrowl("Изменения успешно сохранены!",
+                    {ele: '#editProviderModal', type: 'success', width: 350});
+            }});
+    },
+
+    render: function(){
+        if (this.model == null)
+            return this;
+
+        this.$el.find('#providerNameInputEdit').val(this.model.get('name'));
+        this.$el.find('#providerUrlInputEdit').val(this.model.get('url'));
+
+        return this;
+    }
+});
+
+app1.ProviderPage = new ProviderPage();
