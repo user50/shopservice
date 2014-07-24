bffa77a5bb7e4fa11feacf9a0e4822fbb1e1f8c7
oleg.oginskiy <oleg@themidnightcoders.com>
2014-07-24 18:09:40 +0300

    add ability to search the client products

---
+++ app/controllers/ClientProductController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import com.shopservice.ProductConditions;
+import com.shopservice.Services;
+import com.shopservice.domain.Product;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ClientProductController extends Controller {
+
+    public static Result find(String clientId, String like, String contain)
+    {
+        if (like != null)
+            return ok(Json.toJson(new ArrayList<Product>()));
+
+        ProductConditions conditions = new ProductConditions();
+
+        if (contain != null)
+            conditions.words = Arrays.asList(contain.split(" "));
+
+        Object response = Services.getProductDAO(clientId).find(conditions);
+
+        return ok(Json.toJson(response));
+    }
+}
