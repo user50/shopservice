3a104de7ed120bc3a9456d94d981c125533966bd
oleg.oginskiy <oleg@themidnightcoders.com>
2014-07-30 11:05:46 +0300

    implemented pagination for operation: GET    /clients/:id/products

--- app/controllers/ClientProductController.java
+++ app/controllers/ClientProductController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.shopservice.ProductConditions;
 import com.shopservice.Services;
+import com.shopservice.assemblers.PaginationResult;
 import com.shopservice.domain.Product;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -13,18 +14,20 @@ import java.util.List;
 
 public class ClientProductController extends Controller {
 
-    public static Result find(String clientId, String like, String contain)
+    public static Result find(String clientId, String like, String contain, Integer offset, Integer limit)
     {
         ProductConditions conditions = new ProductConditions();
+        conditions.offset = offset;
+        conditions.limit = limit;
 
         if (contain != null)
             conditions.words = Arrays.asList(contain.split(" "));
 
         List response = Services.getProductDAO(clientId).find(conditions);
+        int total = Services.getProductDAO(clientId).size(conditions);
 
-        if (like != null)
-            return ok(Json.toJson(response.subList(0, 10)));
+        PaginationResult result = new PaginationResult(total, response);
 
-        return ok(Json.toJson(response));
+        return ok(Json.toJson(result));
     }
 }
