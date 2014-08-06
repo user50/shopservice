5acd5abe96538a2ab2a88fc98625ffc9a3ed6bbe
Eugene Lozov <eugene.lozov@sandsiv.com>
2014-08-06 15:44:10 +0300

    ProductSource implementation for the Florange is done

--- app/com/shopservice/productsources/Florange.java
+++ app/com/shopservice/productsources/Florange.java
@@ -1,16 +1,159 @@
 package com.shopservice.productsources;
 
 import com.shopservice.domain.Product;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import play.Logger;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by user50 on 06.08.2014.
  */
 public class Florange implements ProductSource {
+    public static final String SITE_URL = "http://florange.ua";
+
     @Override
     public List<Product> get(Integer providerId) {
-        //TODO
-        return null;
+        List<Product> products = new ArrayList<>();
+
+        try {
+            for (String productUrl : productUrls()){
+                products.addAll(getProducts(productUrl));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        return products;
     }
+
+    private List<Product> getProducts(String url) throws IOException {
+        System.out.println("Parsing of the " + url);
+        List<Product> products = new ArrayList<>();
+
+        try{
+            Document doc = Jsoup.connect(url).get();
+
+            String generalName = doc.select(".name").first().text();
+
+            Elements tabs = doc.select(".tabs > li");
+
+            Elements prices = null;
+            if (doc.select(".oldprice").isEmpty())
+                prices = doc.select(".newprice1");
+            else
+                prices = doc.select(".newprice");
+
+            int i = 0;
+            for (Element tabLi : tabs){
+                Product product = new Product();
+
+                Element tabImg = tabLi.getElementsByTag("img").get(0);
+                Attributes attr = tabImg.attributes();
+                String productName = attr.get("title") + " " + generalName;
+
+                String productPrice = prices.get(i).text().replace("грн.", "");
+                if (productPrice.matches( ". шт -.*" ))
+                    productPrice = productPrice.split("-")[1].trim();
+
+                Elements images = doc.select("ul#slide > li > img");
+                String imageUrl = null;
+                if (!images.isEmpty())
+                    imageUrl = images.get(i).attributes().get("src");
+
+                product.id = String.valueOf(productName.hashCode());
+                product.name = productName;
+                product.price = Double.valueOf(productPrice.replace(",", "."));
+                product.imageUrl = imageUrl;
+
+                products.add(product);
+
+                i++;
+            }
+        } catch (Exception e){
+            Logger.error("Error while parsing " + url);
+        }
+        return  products;
+    }
+
+    public List<String> productUrls () throws IOException {
+        List<String> sectionLinks = getSectionsUrls();
+        List<String> categoriesLinks = new ArrayList<>();
+        List<String> productLinks = new ArrayList<>();
+
+        for (String sectionLink : sectionLinks){
+            categoriesLinks.addAll(getCategoryUrls(sectionLink));
+        }
+
+        for (String categoryLink : categoriesLinks){
+            productLinks.addAll(getProductUrls(categoryLink));
+        }
+
+        return productLinks;
+    }
+
+    private List<String> getSectionsUrls() throws IOException {
+        List<String> urls = new ArrayList<>();
+
+        Document doc = Jsoup.connect(SITE_URL + "/ru/production/catalog/").get();
+
+        Elements sections = doc.select(".nav > li > a");
+
+        for (Element section : sections){
+            String sectionUrl = section.attributes().get("href");
+            urls.add(SITE_URL + sectionUrl);
+        }
+        return urls;
+    }
+
+    private List<String> getCategoryUrls(String sectionUrl) throws IOException {
+        List<String> urls = new ArrayList<>();
+
+        if (sectionUrl.equals(""))
+            return urls;
+
+        Document sectionPage = Jsoup.connect(sectionUrl).get();
+
+        Elements categories = null;
+        if (!sectionPage.select(".cat_onelevel").isEmpty())
+            categories = sectionPage.select(".cat_onelevel").first().children();
+        else
+            categories = sectionPage.select(".cat").first().children();
+
+        for (Element category : categories){
+            String categoryUrl = category.child(0).attributes().get("href");
+            urls.add(SITE_URL + categoryUrl);
+        }
+
+        return urls;
+    }
+
+    private List<String> getProductUrls(String categoryUrl) throws IOException {
+        List<String> urls = new ArrayList<>();
+
+        if (categoryUrl.equals(""))
+            return urls;
+
+        Document categoryPage = Jsoup.connect(categoryUrl).get();
+
+        if (!categoryPage.select(".cat_onelevel").isEmpty()){
+            urls.add(categoryUrl);
+            return urls;
+        }
+
+        Elements products = categoryPage.select(".cat").get(1).children();
+        for (Element product : products){
+            String productUrl = product.child(0).attributes().get("href");
+            urls.add(SITE_URL + productUrl);
+        }
+
+        return urls;
+    }
+
 }
--- test/com/shopservice/FlorangeTest.java
+++ test/com/shopservice/FlorangeTest.java
@@ -1,65 +1,172 @@
 package com.shopservice;
 
 import com.shopservice.domain.Product;
+import com.shopservice.productsources.Florange;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
-import scala.util.parsing.combinator.testing.Str;
+import play.Logger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user50 on 05.08.2014.
  */
 public class FlorangeTest {
-
+    public static final String SITE_URL = "http://florange.ua";
     @Test
     public void testName() throws Exception {
-        Document doc = Jsoup.connect("http://florange.ua/ru/production/catalog/basic2014/dana/").get();
+        List<Product> products = new ArrayList<>();
 
-        String generalName = doc.select(".name").first().text();
-
-        Elements tabs = doc.select(".tabs");
-
-        Elements prices = null;
-        if (doc.select(".oldprice").isEmpty())
-            prices = doc.select(".newprice1");
-        else
-            prices = doc.select(".newprice");
-
-        int i = 0;
-        for (Element tabLi : tabs.get(0).children()){
-            Product product = new Product();
-
-            String id = tabLi.id();
-            Element span = tabLi.getElementsByTag("span").get(0);
-            Attributes attr = span.child(0).attributes();
-            String productName = attr.get("title") + " " + generalName;
-            String productPrice = prices.get(i).text().replace("грн.", "").trim();
-            String imageUrl = doc.select("#slide").get(0).children().get(i).children().first().attributes().get("src");
-
-            product.name = productName;
-            product.price = Double.valueOf(productPrice);
-            product.imageUrl = imageUrl;
-
-            i++;
+        for (String productUrl : productUrls()){
+            products.addAll(getProducts(productUrl));
         }
 
+        System.out.println();
+    }
+
+    private List<Product> getProducts(String url) throws IOException {
+        System.out.println("Parsing of the " + url);
+        List<Product> products = new ArrayList<>();
+
+        try{
+            Document doc = Jsoup.connect(url).get();
+
+            String generalName = doc.select(".name").first().text();
+
+            Elements tabs = doc.select(".tabs > li");
+
+            Elements prices = null;
+            if (doc.select(".oldprice").isEmpty())
+                prices = doc.select(".newprice1");
+            else
+                prices = doc.select(".newprice");
+
+            int i = 0;
+            for (Element tabLi : tabs){
+                Product product = new Product();
+
+                String id = tabLi.id();
+                Element tabImg = tabLi.getElementsByTag("img").get(0);
+                Attributes attr = tabImg.attributes();
+                String productName = attr.get("title") + " " + generalName;
+
+                String productPrice = prices.get(i).text().replace("грн.", "");
+                if (productPrice.matches( ". шт -.*" ))
+                    productPrice = productPrice.split("-")[1].trim();
+
+                Elements images = doc.select("ul#slide > li > img");
+                String imageUrl = null;
+                if (!images.isEmpty())
+                    imageUrl = images.get(i).attributes().get("src");
+
+                product.name = productName;
+                product.price = Double.valueOf(productPrice.replace(",", "."));
+                product.imageUrl = imageUrl;
+
+                products.add(product);
+
+                i++;
+            }
+        } catch (Exception e){
+            Logger.error("Error while parsing " + url);
+        }
+        return  products;
+    }
+
+    public List<String> productUrls () throws IOException {
+        List<String> sectionLinks = getSectionsUrls();
+        List<String> categoriesLinks = new ArrayList<>();
+        List<String> productLinks = new ArrayList<>();
+
+        for (String sectionLink : sectionLinks){
+            categoriesLinks.addAll(getCategoryUrls(sectionLink));
+        }
+
+        for (String categoryLink : categoriesLinks){
+            productLinks.addAll(getProductUrls(categoryLink));
+        }
+
+        return productLinks;
+    }
+
+    private List<String> getSectionsUrls() throws IOException {
+        List<String> urls = new ArrayList<>();
+
+        Document doc = Jsoup.connect(SITE_URL + "/ru/production/catalog/").get();
+
+        Elements sections = doc.select(".nav > li > a");
+
+        for (Element section : sections){
+            String sectionUrl = section.attributes().get("href");
+            urls.add(SITE_URL + sectionUrl);
+        }
+        return urls;
+    }
+
+    private List<String> getCategoryUrls(String sectionUrl) throws IOException {
+        List<String> urls = new ArrayList<>();
+
+        if (sectionUrl.equals(""))
+            return urls;
+
+        Document sectionPage = Jsoup.connect(sectionUrl).get();
+
+        Elements categories = null;
+        if (!sectionPage.select(".cat_onelevel").isEmpty())
+            categories = sectionPage.select(".cat_onelevel").first().children();
+        else
+            categories = sectionPage.select(".cat").first().children();
+
+        for (Element category : categories){
+            String categoryUrl = category.child(0).attributes().get("href");
+            urls.add(SITE_URL + categoryUrl);
+        }
+
+        return urls;
+    }
+
+    private List<String> getProductUrls(String categoryUrl) throws IOException {
+        List<String> urls = new ArrayList<>();
+
+        if (categoryUrl.equals(""))
+            return urls;
+
+        Document categoryPage = Jsoup.connect(categoryUrl).get();
+
+        if (!categoryPage.select(".cat_onelevel").isEmpty()){
+            urls.add(categoryUrl);
+            return urls;
+        }
+
+        Elements products = categoryPage.select(".cat").get(1).children();
+        for (Element product : products){
+            String productUrl = product.child(0).attributes().get("href");
+            urls.add(SITE_URL + productUrl);
+        }
+
+        return urls;
     }
 
     @Test
     public void testName1() throws Exception {
-        Document doc = Jsoup.connect("http://florange.ua/ru/production/catalog/").get();
+        List<Product> products = getProducts("http://florange.ua/ru/production/catalog2/dolcevita/bell/");
+    }
 
-        Elements categories = doc.select(".nav").first().children();
+    @Test
+    public void testProductsUrl() throws Exception {
+        productUrls();
+    }
 
-        for (Element category : categories){
-            String url = category.child(0).attributes().get("href");
-            System.out.println(url);
-        }
-
-
+    @Test
+    public void testGet() throws Exception {
+        Florange florangeProductSource = new Florange();
+        List<Product> products = florangeProductSource.get(null);
 
     }
 }
