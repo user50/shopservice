1ad0bb458aaca35b7e0a35dc4c93d729469dee5c
user50 <oor1@ukr.net>
2014-08-06 09:12:02 +0300

    added florange product source

---
+++ app/com/shopservice/productsources/Florange.java
@@ -0,0 +1,16 @@
+package com.shopservice.productsources;
+
+import com.shopservice.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by user50 on 06.08.2014.
+ */
+public class Florange implements ProductSource {
+    @Override
+    public List<Product> get(Integer providerId) {
+        //TODO
+        return null;
+    }
+}
