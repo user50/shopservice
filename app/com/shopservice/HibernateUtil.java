01fc11fd360a6ece1cee618257905fd6d4b2588d
user50 <oor1@ukr.net>
2014-11-16 16:19:48 +0200

    hikari hibernate

--- app/com/shopservice/HibernateUtil.java
+++ app/com/shopservice/HibernateUtil.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
@@ -13,11 +14,11 @@ public class HibernateUtil {
 
     static {
         try {
-//            pool = new HikariConnectionPool("jdbc:mysql://us-cdbr-east-05.cleardb.net:3306/heroku_20e5b087480e48d?useUnicode=yes&characterEncoding=utf8" +
-//                    "&user=b02276676df1a5&password=2c270044");
+            pool = new HikariConnectionPool("jdbc:mysql://us-cdbr-east-05.cleardb.net:3306/heroku_20e5b087480e48d?useUnicode=yes&characterEncoding=utf8" +
+                    "&user=b02276676df1a5&password=2c270044");
 
-            pool = new HikariConnectionPool("jdbc:mysql://localhost:3306/shopservice?" +
-                    "user=root&password=neuser50");
+//            pool = new HikariConnectionPool("jdbc:mysql://localhost:3306/shopservice?" +
+//                    "user=root&password=neuser50");
 
             Configuration configuration = new Configuration();
             addAnnotatedClasses( configuration );
@@ -48,7 +49,7 @@ public class HibernateUtil {
     {
         configuration
                 .setProperty("show_sql", "true")
-                .setProperty( "dialect", "org.hibernate.dialect.MySQLDialect" );
+                .setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
 //                .setProperty( "hibernate.c3p0.min_size", "5" )
 //                .setProperty( "hibernate.c3p0.max_size", "20" )
 //                .setProperty( "hibernate.c3p0.timeout", "1800" )
