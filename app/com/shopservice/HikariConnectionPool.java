6d6cc607a98f9e2cb68671c1cd5dad20b0d873cb
user50 <oor1@ukr.net>
2014-12-02 21:36:28 +0200

    increase hibernate connection pool capacity to 5

--- app/com/shopservice/HikariConnectionPool.java
+++ app/com/shopservice/HikariConnectionPool.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
  */
 public class HikariConnectionPool implements ConnectionPool {
 
-    private static final int CONNECTION_POOL_SIZE = 1;
+    private static final int CONNECTION_POOL_SIZE = 5;
 
     DataSource dataSource;
 
