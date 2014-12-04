b0f8171abdcc48ee7d0ebcd62c6e915ce8150282
Oleg Oginskyi <oor1@ukr.net>
2014-12-04 17:00:45 +0200

    use play's connection pool

--- app/com/shopservice/HibernateUtil.java
+++ app/com/shopservice/HibernateUtil.java
@@ -6,6 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import play.db.DB;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
@@ -14,17 +15,18 @@ public class HibernateUtil {
 
     static {
         try {
-            pool = new HikariConnectionPool("jdbc:mysql://us-cdbr-east-05.cleardb.net:3306/heroku_20e5b087480e48d?useUnicode=yes&characterEncoding=utf8" +
-                    "&user=b02276676df1a5&password=2c270044");
+//            pool = new HikariConnectionPool("jdbc:mysql://us-cdbr-east-05.cleardb.net:3306/heroku_20e5b087480e48d?useUnicode=yes&characterEncoding=utf8" +
+//                    "&user=b02276676df1a5&password=2c270044");
 
 //            pool = new HikariConnectionPool("jdbc:mysql://localhost:3306/shopservice?" +
 //                    "user=root&password=neuser50");
+
 
             Configuration configuration = new Configuration();
             addAnnotatedClasses( configuration );
             addProperties( configuration );
             StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-            sessionFactory = configuration.buildSessionFactory(ssrb.applySetting(Environment.DATASOURCE, pool.dataSource).build());
+            sessionFactory = configuration.buildSessionFactory(ssrb.applySetting(Environment.DATASOURCE, DB.getDataSource()).build());
 
         } catch (ExceptionInInitializerError ex) {
             System.err.println("Initial SessionFactory creation failed: " + ex);
@@ -47,7 +49,7 @@ public class HibernateUtil {
     {
         configuration
                 .setProperty("show_sql", "true")
-                .setProperty( "dialect", "org.hibernate.dialect.MySQLDialect" );
+                .setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
 //                .setProperty( "hibernate.c3p0.min_size", "5" )
 //                .setProperty( "hibernate.c3p0.max_size", "20" )
 //                .setProperty( "hibernate.c3p0.timeout", "1800" )
